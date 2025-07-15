3b7295bc016c56429285201323e0897b1eddeacc
Maia Fattori Abati <maiaabati@gmail.com>
2025-07-15 14:02:22 -0300

    Added some new naviagation pages, and the image

---
+++ MindBloom/FutureFeatures.swift
@@ -0,0 +1,18 @@
+//
+//  FutureFeatures.swift
+//  MindBloom
+//
+//  Created by Maia Fattori Abati on 15/07/25.
+//
+
+import SwiftUI
+
+struct FutureFeatures: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FutureFeatures()
+}
