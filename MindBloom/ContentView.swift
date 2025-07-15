e8f79cae3efa467f517cb6a4792bef9541244770
Maia Fattori Abati <maiaabati@gmail.com>
2025-07-15 10:14:48 -0300

    Initial Commit

---
+++ MindBloom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindBloom
+//
+//  Created by Maia Fattori Abati on 15/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindBloom/MindBloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindBloomApp.swift
+//  MindBloom
+//
+//  Created by Maia Fattori Abati on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MindBloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
